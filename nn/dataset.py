6ab49e73e56e3dd090ea4cc14df42f36dafbeabb
Marcel88888 <marcel.kawski1@gmail.com>
2021-04-25 12:54:20 +0200

    Train model

--- nn/dataset.py
+++ nn/dataset.py
@@ -7,15 +7,15 @@ class PurchaseDataset(Dataset):
     def __init__(self, data_csv_file, train):
         data = pd.read_csv(data_csv_file)
         if train is True:
-            self.data = data.iloc[:7678]
+            self.data = data.iloc[:8000]
         else:
-            self.data = data.iloc[7678:]
+            self.data = data.iloc[8000:]
         self.train = train
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        stats = torch.tensor(self.data.loc[:, self.data.columns != 'successful'])
-        success = torch.tensor(self.data['successful'])
+        stats = torch.tensor(self.data.iloc[idx, self.data.columns != 'successful'].values)
+        success = torch.tensor(self.data.iloc[idx]['successful'])
         return {"stats": stats, "success": success}
