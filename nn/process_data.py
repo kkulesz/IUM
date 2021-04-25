ac99bd49db68bebe3a9ab4690248dd799ac477ce
Marcel88888 <marcel.kawski1@gmail.com>
2021-04-25 12:18:27 +0200

    Process data for model

---
+++ nn/process_data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def encode_boolean(b):
+    if b is False:
+        return 0
+    else:
+        return 1
+
+
+def convert_length(leng):
+    days, time = leng.split('days')
+    days = int(days)
+    seconds = days * 86400  # 1 day = 86400 s
+    h, m, s = time.split(':')
+    h, m, s = int(h), int(m), int(s)
+    seconds += h * 3600
+    seconds += m * 60
+    seconds += s
+    return seconds
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/parsed_data.csv')
+    data.drop(data.columns[[0]], axis=1, inplace=True)
+    data = data.drop(['user_id', 'city'], axis=1)
+    data['length'] = data['length'].apply(convert_length)
+    data['successful'] = data['successful'].apply(encode_boolean)
+    data['gender'] = data['gender'].apply(encode_boolean)
+    print(data.info())
+    # data.to_csv('data/data.csv', index=None)
